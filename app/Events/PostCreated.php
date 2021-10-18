aa5da686417bc75e1b3ee026b0c230cfc64085a7
Marcos Valverde <marcos.valverde@infosistemas.com.br>
2021-10-17 23:52:00 -0300

    Broadcast Created event e listener

--- app/Events/PostCreated.php
+++ app/Events/PostCreated.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PostCreated
+class PostCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -34,6 +34,6 @@ class PostCreated
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('post-created');
     }
 }
